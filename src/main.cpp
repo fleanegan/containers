0448b3f8fa77ca9221ec6f2544ce2c73355b33fb
te <testsetestsetestsaegsr@trash-mail.com>
2022-06-16 09:14:33 +0200

    feat(iterator traits): defining iterator outside the actual class

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,8 @@
 
 #ifndef TESTING
 
-
-template <typename T>
-class Exp1{
-public:
-	class Iterator : public ft::iterator<ft::input_iterator_tag, T, std::ptrdiff_t, T*, T&>{
-
-	};
-	Exp1() : iterator() {}
-
-	Iterator iterator;
-};
-
-template <typename T>
-class Exp2{
-public:
-	class Iterator : public ft::iterator<ft::bidirectional_iterator_tag, T, std::ptrdiff_t, T*, T&>{
-
-	};
-	Exp2() : iterator() {}
-
-	Iterator iterator;
-};
-
-
 int	main(){
-	std::vector<int> v(2,3);
-	Exp1<int> objectWithWeakIterator;
-	Exp2<int> objectWithStrongIterator;
-	foo<Exp1<int>::Iterator>(objectWithWeakIterator.iterator); // will call the specialized version
-	foo<Exp2<int>::Iterator>(objectWithStrongIterator.iterator); // will call the specialized version
-	return (0);
+
 }
 
 #endif

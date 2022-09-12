245f9b1ba3c5f691211101028faa26657c634764
Frederik SCHLUTER <fschlute@e1r9p2.clusters.42paris.fr>
2022-09-12 13:28:58 +0200

    feat(BinarySearchTree): example main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,217 +1,120 @@
-#include <iostream>
-#include "iterator_traits.hpp"
-#include "../inc/iterator_traits.hpp"
-#include "../inc/vector.hpp"
-#include "../inc/VectorIterator.hpp"
-#include "../inc/RedBlackTree.hpp"
-#include <iterator>
-#include <vector>
-#include <typeinfo>
-#include <map>
-#include "stack.hpp"
-#include "pair.hpp"
-#include "BinarySearchTree.hpp"
-#include "map.hpp"
-#include "MapIterator.hpp"
-
-#define TESTED_TYPE int
-#ifndef TESTED_NAMESPACE
-#define TESTED_NAMESPACE ft
-#endif
-
 #ifndef TESTING
-#include <vector>
+#include <iostream>
+#include <string>
+#include <deque>
+#if 0 //CREATE A REAL STL EXAMPLE
+#include <map>
 #include <stack>
-#include <sstream>
-#include "../test/testUtils.hpp"
-
-//#define STD
-#ifndef STD
-# define NAMESPACE ft
+#include <vector>
+namespace ft = std;
 #else
-# define NAMESPACE std
+#include <map.hpp>
+	#include <stack.hpp>
+	#include <vector.hpp>
 #endif
 
-using namespace NAMESPACE;
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   map_prelude.hpp                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/08 20:49:46 by mleblanc          #+#    #+#             */
-/*   Updated: 2022/05/24 13:27:12 by mleblanc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdlib.h>
 
-
-#include "map.hpp"
-#include <cstddef>
-#include <functional>
-#include <map>
-#include <string>
-
-
-template <typename It>
-void print_map(It first, It last)
+#define MAX_RAM 4294967296
+#define BUFFER_SIZE 4096
+struct Buffer
 {
-	for (; first != last; ++first) {
-		std::cout << "K: " << first->first << " V: " << first->second << " ";
+	int idx;
+	char buff[BUFFER_SIZE];
+};
+
+
+#define COUNT (MAX_RAM / (int)sizeof(Buffer))
+
+template<typename T>
+class MutantStack : public ft::stack<T>
+{
+public:
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& src) { *this = src; }
+	MutantStack<T>& operator=(const MutantStack<T>& rhs)
+	{
+		this->c = rhs.c;
+		return *this;
+	}
+	~MutantStack() {}
+
+	typedef typename ft::stack<T>::container_type::iterator iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+};
+
+int main(int argc, char** argv) {
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./test seed" << std::endl;
+		std::cerr << "Provide a seed please" << std::endl;
+		std::cerr << "Count value:" << COUNT << std::endl;
+		return 1;
+	}
+	const int seed = atoi(argv[1]);
+	srand(seed);
+
+	ft::vector<std::string> vector_str;
+	ft::vector<int> vector_int;
+	ft::stack<int> stack_int;
+	ft::vector<Buffer> vector_buffer;
+	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
+	ft::map<int, int> map_int;
+
+	for (int i = 0; i < COUNT; i++)
+	{
+		vector_buffer.push_back(Buffer());
+	}
+
+	for (int i = 0; i < COUNT; i++)
+	{
+		const int idx = rand() % COUNT;
+		vector_buffer[idx].idx = 5;
+	}
+	ft::vector<Buffer>().swap(vector_buffer);
+
+	try
+	{
+		for (int i = 0; i < COUNT; i++)
+		{
+			const int idx = rand() % COUNT;
+			vector_buffer.at(idx);
+			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
+		}
+	}
+	catch(const std::exception& e)
+	{
+		//NORMAL ! :P
+	}
+
+	for (int i = 0; i < COUNT; ++i)
+	{
+		map_int.insert(ft::make_pair(rand(), rand()));
+	}
+
+	int sum = 0;
+	for (int i = 0; i < 10000; i++)
+	{
+		int access = rand();
+		sum += map_int[access];
+	}
+	std::cout << "should be constant with the same seed: " << sum << std::endl;
+
+	{
+		ft::map<int, int> copy = map_int;
+	}
+	MutantStack<char> iterable_stack;
+	for (char letter = 'a'; letter <= 'z'; letter++)
+		iterable_stack.push(letter);
+	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
+	{
+		std::cout << *it;
 	}
 	std::cout << std::endl;
-}
-
-#define PRINT_MAP(map)                                                                             \
-    {                                                                                              \
-        std::cout << "\nMap content:\n";                                                           \
-        print_map(map.begin(), map.end());                                                         \
-        std::cout << std::endl;                                                                    \
-    }
-
-#define PRINT_ALL(map)                                                                             \
-    {                                                                                              \
-        PRINT_SIZE(map);                                                                           \
-        PRINT_MAP(map);                                                                            \
-    }
-
-#define PRINT_PAIR_PTR(p)                                                                          \
-    {                                                                                              \
-        PRINT_LINE("Key:", (p)->first);                                                            \
-        PRINT_LINE("Value:", (p)->second);                                                         \
-    }
-
-#define PRINT_PAIR_REF(p)                                                                          \
-    {                                                                                              \
-        PRINT_LINE("Key:", (p).first);                                                             \
-        PRINT_LINE("Value:", (p).second);                                                          \
-    }
-
-template <typename T>
-void init_array_int_str(NAMESPACE::pair<int, std::string>* arr, std::size_t size)
-{
-	std::string x[64] = { "CBCId4lUU6oYms60JkNr", "V5sbXjlqYRAV1C1LgJbt", "TPcK94z2htaoK5hWpR1j",
-						  "WA1JMsjadjyjQjJLxVkE", "aDhJUsH7AVnKLfzQGcPC", "DPpRIm6G9C5CWSWYLwnt",
-						  "aKyxvwuxzUAksmm7s8o3", "d8NpBFRFI87WxlYiRzJB", "5RDlRrY8OFBkqtlD5R33",
-						  "W4dsiGdNWranxT1qbPt9", "uUxBDNzk3Tpg1bIow0uf", "FiKAXgTFhhPhmRkfXci9",
-						  "02Fxrj1KnMufOpH1gbxw", "hSV7LrNPzTKHS6h7BRW6", "YzIjmI4i4Fum1gzs9QFy",
-						  "6jjGwGu20C7dXFRgdCU8", "bej1zTuyCJGa0b4KXgQO", "ckkK36lt3kBVH1PYvbib",
-						  "Gs9kLamEeSWqM9ORSlzF", "oJTGXtRpq4rz6IBt2Gc2", "6XSQM096yPnzWqydO7Fl",
-						  "Kw91n77jigS09IQEYFw0", "K0Qj3EOV96U6xbBDMkaP", "i0b4NYygRb7ZuT3HOE61",
-						  "C6KyCaoRm5MncGCjjP0I", "6xxl5c88m0Rq5CkTPsTU", "xtZ16NJzNU4IEnzsily8",
-						  "2ofwuG2e80JGsK8VdB6r", "UjhRe94EahPlXuBbIV0q", "be5WTfjjKVBU8cezAjH8",
-						  "zOWJPAQLV4wgVGJTC0uV", "nuqgXoNhZpL8uMvbDcT7", "HdCk0Gm8Gn3eqcVYxcYD",
-						  "PuIS5xPbBpGe3MHG7SNm", "ZY6finqJMbHpB6dKlenn", "VJfBTOFj9reLm3iGU2Fs",
-						  "UFWOX8UzTSzSAGMvGRIC", "cZfcw5yVwFprkH2v3FQO", "V34DSjjaiDianUKjRFCj",
-						  "j7SHtomH6GvAY8y0nL9E", "3z82NvciqTwWW2Lp7MET", "S1BDCdeUHLIVB6I8zaIc",
-						  "OkuQPNHq0AnYY9Pemxpn", "yH5J2tI4oQzVsPBB226z", "aafwp6lvuOxv5JkUBUlA",
-						  "RjRwqlQ0hnxYnglid2x3", "3RBzhOIHFtRtZBHNlCVv", "lttYCjRWSXYKeyIgvpJv",
-						  "6KhIc7LvaTTwklmmbjBO", "IDlAqFCEdFTJcVW36RsY", "pDGkqtvKVRReq19BFVwN",
-						  "88UJAeNtrlAwEu4BGiSb", "eFN5bxsWa4dAnvZdzToE", "B3cqY0IqO2IH2yGMnX7e",
-						  "BRcU6oFTAC8IWuiSrTEa", "dtBZVLQMx9MBw1eak8SB", "zM2XVrazn2fXAsRHDc16",
-						  "qqlM2sERpGhcZJAv3qPR", "4Lru0dOIQpWGYlcx9oyn", "HSH0O0igQruyLC1FmQVs",
-						  "mXHcnXiXhKJwJ0BUSpje", "sXhbSe7MQHhvJ9XQcrrX", "aTejup4iQw70n78erKEB",
-						  "1IRLC9sfQI5AzDYeotLd" };
-
-	for (std::size_t i = 0; i < size; ++i) {
-		arr[i] = NAMESPACE::make_pair(i, x[i]);
-	}
-}
-
-template <typename T>
-void init_array_str_str(NAMESPACE::pair<std::string, std::string>* arr, std::size_t size)
-{
-	std::string x[32] = { "RxWAUMME3GtF9Zbv3a4M", "WC96vs7Z6I6TfdGxObUg", "2beuhrfBZvjdkmfqV9yn",
-						  "AuIhF3lUgwT0OMy8DuUx", "dVW2KAL06gB4s7rEf508", "jnsp7MGfl5wZNZVfQSvp",
-						  "p26Kfhz0Iof6rJDy27vc", "bnWXfp2eexq7MPpcmRSI", "tMEU0L6RNvqDAaV6HN0e",
-						  "POcbaPteUMFfltrwdu7r", "epDGhJZoKoL4i1HO67XS", "o83N1RTIeOafqBfqeAYh",
-						  "aYRtwuDT8k7k7iIfTrtU", "fEPXIREoFGKXqnNKySG5", "QrqmIbVVIQzf8WKBxiga",
-						  "k5NSg0Dq0b9ewfrhYdy7", "Tf9dsqBADfMeWVQGG3Zk", "dS2LouElpUXsXNCClr4B",
-						  "tsvf3JaWNg5JlSDZs1Dm", "oWurB8fBAIBQq1ZnlFiR", "8YllXP80XunEXhqnjkZc",
-						  "Spt36tVcEkIFjwogWli0", "c25H1u6DF9RVdagn81DA", "AjWfT7TlXgzSKOywpoTH",
-						  "ZwTg8WNnmXOJKdW6ZeJv", "E9aYpoCXd1kbSWhdLOnf", "QN1iAuoSNQ5WvHeSeSSL",
-						  "7g5wfNllowIaqMUVjywr", "b38gJrosdk9xEQ2K8Gbu", "KdVdRVLHJg9Bazc4cpM7",
-						  "COJeGmQEzjXwqKCAnd6J", "2k6XCpXEkP7QoST6uYoy" };
-
-	std::string y[32] = { "TZ3QuE3ZNUbFrEpQhgcY", "BX2lyPzOWRIznb34T5OT", "v7lPrSeDokdBIUXR9QGh",
-						  "nAQxkuEYL5k1Er7FspKu", "r8T4oZaWBI1m7sPBEDz3", "pdxM9tJrYZskZla7L7yM",
-						  "dIIx6eKTfygu24SNKa32", "tw04BOo705mdWxuDciNO", "q94icFprhKrrFXLvjEN3",
-						  "uM25BWUl14NTlTwFsNIv", "2oTV9fXUESLQqzvsPFmd", "W6ASLRzrlOggx5b7CtYN",
-						  "bz4BQ7rLU203RNwwmbMK", "jfWgRPiuy1P3NGDPWmbe", "PgtdYLdGuneYEUgMA8SB",
-						  "eaaFugCSOOU5my4y4SkG", "gGd4FiUEJwgvhOCSHu9g", "9K0ZhqlbyDCJXYXHoEIN",
-						  "VTmk8m7rbEGl8ntGpTCQ", "eRJ98PUF2tSMX6zpDOi4", "wFMJIDAbjYnJkxMxkAtL",
-						  "JCOXV4iPcIt72DcNnbuY", "LsnOUrHHD2FNFH5DMvbc", "YVPp2jPqK3jmif7yY14r",
-						  "JBMhRRtLY3QX3x1ZVDCs", "Uwo5PD2RLMx5ByW9Rscq", "O4Q6DPBJHOelMtBKc9GQ",
-						  "Bq0H2eqnuCYqyj9F5Lkn", "P2fJf0TYN92iUXkwELjG", "f656ltXlumpc7YyPVjan",
-						  "YQfr76LAA1xKokfq8HpL", "6utvLeZ9t0IM4OkdfAQ2" };
-
-	for (std::size_t i = 0; i < size; ++i) {
-		arr[i] = NAMESPACE::make_pair(x[i], y[i]);
-	}
-}
-
-#define SETUP_ARRAYS()                                                                             \
-    NAMESPACE::pair<int, std::string> intstr_arr[64];                                              \
-    init_array_int_str<void>(intstr_arr, 64);                                                      \
-    NAMESPACE::pair<std::string, std::string> strstr_arr[32];                                      \
-    init_array_str_str<void>(strstr_arr, 32);                                                      \
-    std::size_t intstr_size = 64;                                                                  \
-    std::size_t strstr_size = 32;                                                                  \
-    (void)intstr_arr;                                                                              \
-    (void)strstr_arr;                                                                              \
-    (void)intstr_size;                                                                             \
-    (void)strstr_size;
-
-typedef NAMESPACE::map<int, std::string, std::less<int>,
-		std::allocator<NAMESPACE::pair<const int, std::string> > >
-intmap;
-typedef NAMESPACE::map<std::string, std::string, std::less<std::string>,
-		std::allocator<NAMESPACE::pair<const std::string, std::string> > >
-strmap;
-
-#define PRINT_BOUND(b, end)                                                                        \
-    {                                                                                              \
-        if (b != end) {                                                                            \
-            PRINT_PAIR_REF(*b);                                                                    \
-        } else {                                                                                   \
-            PRINT_MSG("End iterator");                                                             \
-        }                                                                                          \
-        std::cout << std::endl;                                                                    \
-    }
-
-#define PRINT_EQ_RANGE(p, end)                                                                     \
-    {                                                                                              \
-        std::cout << "--------------------------------\n";                                         \
-        PRINT_BOUND(p.first, end);                                                                 \
-        PRINT_BOUND(p.second, end);                                                                \
-    }
-
-
-typedef NAMESPACE::map<std::string, std::string, std::less<std::string>,
-		std::allocator<NAMESPACE::pair<const std::string, std::string> > >
-strmap;
-
-int main()
-{
-	ft::RedBlackTree<int, int, ft::RedBlackNode<int, int> >rbt;
-	rbt.insertByValue(ft::make_pair(int(1), int(0)));
-	rbt.insertByValue(ft::make_pair(int(2), int(0)));
-	std::cout << "\n\n\nnn\n\n\n\nn\nn\nn\n\n\n";
-	rbt.insertByValue(ft::make_pair(int(3), int(0)));
-	std::cout << "\n\n\nneeeeeeeeeeeeeeeeeeeeeeen\n\n\n\nn\nn\nn\n\n\n";
-//	rbt.insertByValue(ft::make_pair(simpleDummy(6), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(0), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(10), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(11), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(9), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(13), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(7), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(12), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(14), simpleDummy(0)));
-//	rbt.insertByValue(ft::make_pair(simpleDummy(19), simpleDummy(0)));
 	return (0);
 }
-
 
 
 #endif
